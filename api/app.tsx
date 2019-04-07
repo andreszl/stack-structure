6701e114bc4352b25978af5444de258b32b8baa9
dubanzl <duban.devcode@gmail.com>
2019-04-07 12:38:20 -0500

    redux

--- api/app.tsx
+++ api/app.tsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import { renderToString } from 'react-dom/server'
-// import { StaticRouter  } from 'react-router-dom'
-// import App from '../client/src/components/app.component'
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
-// import reducers from '../client/src/reducers'
-
 export function handleRender(req, res) {
-            
-        // const  store : any = createStore(reducers)
-
-        // const html  = renderToString(
-        // <Provider store={store}>
-        //         <StaticRouter location={req.url} context={{}}>
-        //                 <App />
-        //         </StaticRouter>
-        // </Provider>              
-        // );
-        
-        // res.render('index', { html: html, initialState: JSON.stringify(store.getState())})
         res.render('index')
 }
