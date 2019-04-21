60c0181aca41d03605c1590a9c7a5295f48c07fb
andreszl <andreszl.devcode@gmail.com>
2019-04-21 13:32:51 -0500

    mongodb

---
+++ api/database/db.conn.ts
@@ -0,0 +1,51 @@
+import { MongoClient, Db } from 'mongodb'; // eslint-disable-line no-unused-vars
+import { isEmpty } from 'lodash';
+
+class Database {
+	private host : string;
+	private port : number;
+	public dbname : string;
+	public connection ?: MongoClient;
+
+
+	constructor() {
+		this.host = 'localhost';
+		this.port = 27017;
+		this.dbname = 'sexus';
+	}
+
+	public async getInstance() {
+		try {
+			if (isEmpty(this.connection)) {
+				await this.connect();
+			}
+			if (this.connection !== undefined) {
+				return this.connection.db(this.dbname);
+			}
+			throw Error;
+		} catch (error) {
+			console.log(`Error get database ${error}`);
+			throw error;
+		}
+	}
+
+	public async connect() {
+		try {
+			this.connection = await MongoClient.connect(`mongodb://${this.host}:${this.port}`, { useNewUrlParser: true });
+		} catch (error) {
+			console.log(`Error in connect database ${error}`);
+		}
+	}
+
+	public disconnect() {
+		try {
+			if (this.connection !== undefined) {
+				this.connection.close();
+			}
+		} catch (error) {
+			console.log(`Error in desconnect database ${error}`);
+		}
+	}
+}
+
+export default new Database().getInstance() as Promise<Db>;
